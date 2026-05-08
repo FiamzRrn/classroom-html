57ed4b31aaa82d617735520a2e34f4a4f2694c4e
FiamzRrn <fikri.fg287@gmail.com>
2026-05-08 15:41:29 +0700

    Update API_URL to production endpoint

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost/tugas_kelompok/api.php";
+const API_URL = "http://backend-fikri.ct.ws/api.php";
 
 document.addEventListener('DOMContentLoaded', () => {
     const savedTheme = localStorage.getItem('themeTI') || 'dark';
